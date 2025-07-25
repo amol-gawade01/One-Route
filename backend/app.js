06449e4de9df62a36fa7a834cb204b4a676726e9
amol <amolgawade56@gmail.com>
2025-07-25 11:46:28 +0530

    Added route handling for users

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,12 @@ const app = express()
 const cors = require('cors');
 app.use(express.json())
 app.use(cors())
+app.use(express.urlencoded({extended:true}));
+
+const userRouter = require('./routes/user.routes')
+
+
+app.use('/api/v1/users',userRouter)
 
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const http = require("http")
 const app = require('./app')
-
+const conneectToDB = require('./DB/db')
 const port = process.env.PORT || 3000
 
 
+
 const server = http.createServer(app)
+conneectToDB();
 
 server.listen(port,() => {
     console.log(`server is running on ${port}`);
